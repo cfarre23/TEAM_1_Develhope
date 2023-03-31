07707b22299cc03dabcc4c9bbbcb70ec38ce7b58
Fred Eugine Adomako <fredadomako2003@gmail.com>
2023-03-31 12:25:46 +0200

    Trying to incorporate\add some javascript

--- jpt.js
+++ jpt.js
@@ -1,8 +1,16 @@
-let buttons = document.querySelectorAll('.scale_borders');
+const button = document.getElementById("btn_trade");
+const scaleBorders = document.querySelector(".scale_borders");
+const options = document.querySelector(".options");
 
-buttons.forEach((button, index) =>{
-    button.addEventListener('mouseover',()=>{
-        button.style.display ='flex'
-    })
+function showOptions() {
+    options.style.visibility = "visible";
 }
-)
+
+function hideOptions() {
+    options.style.visibility = "hidden";
+}
+
+button.addEventListener("mouseover", showOptions);
+scaleBorders.addEventListener("mouseover", showOptions);
+button.addEventListener("mouseout", hideOptions);
+scaleBorders.addEventListener("mouseout", hideOptions);
