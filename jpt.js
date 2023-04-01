cf149bc2eadee785d965d2fd1910f14d009fb792
Fred Eugine Adomako <fredadomako2003@gmail.com>
2023-04-02 01:44:43 +0200

    80% done with the hovering effect

--- jpt.js
+++ jpt.js
@@ -1,7 +1,20 @@
 const buttons = document.querySelectorAll(".buttons");
 const options = document.querySelectorAll(".options");
 const images = document.querySelectorAll('.imgs');
+const bnbText = document.querySelector('.bnb_word')
 
+
+//Media query to remove a word
+function good(){
+if(window.innerWidth <= 1127){
+    bnbText.innerHTML = 'BNB';
+}
+else {
+    bnbText.innerHTML = 'BNB Smart Chain';
+}
+}
+window.onload = good;
+window.onresize = good;
 
 buttons.forEach((button,index) =>{
     let timeoutId;
@@ -18,36 +31,51 @@ buttons.forEach((button,index) =>{
         timeoutId = setTimeout(() => {
             hideBoth();
         }, 40);
+        divs.addEventListener('mouseout', () => {
+            hideBoth();
+        });
     });
 
-    divs.addEventListener('mouseout', () => {
-            hideBoth();
-    });
 
     // Function to hide the div
     function hideBoth() {
-        // Hide the div only if both the button and the div are not being hovered over
-        if (!button.matches(':hover') && !divs.matches(':hover')) {
+        // Only hide the div if both the button and the div are not currently being hovered over
+        const buttonNotHovered = !button.matches(':hover');
+        const divNotHovered = !divs.matches(':hover');
+
+        if (buttonNotHovered && divNotHovered) {
             divs.style.visibility = 'hidden';
         }
     }
+
 })
-images.forEach((image, index) =>{
+images.forEach((image, index) => {
     image.addEventListener('mouseover', function todo() {
-        if (index == 2){
+        if (index == 2) {
             options[6].style.visibility = "visible";
             options[6].style.display = "flex";
-            options[5].style.display = 'flex'
+            options[6].addEventListener("mouseover", function showOptions() {
+                options[6].style.visibility = "visible";
+            });
+            options[5].style.display = 'flex';
+            options[5].style.marginLeft = '-5rem';
         }
     })
-    image.addEventListener('mouseout', function remove(){
-        if (index == 2){
-            options[6].style.visibility = "hidden";
-            options[6].style.display = "none";
+
+    image.addEventListener('mouseout', function () {
+        setTimeout(remove, 40);
+        options[6].addEventListener('mouseout', () => {
+            remove();
+        });
+    });
+
+    function remove() {
+        if (index == 2) {
+            if (!image.matches(':hover') && !options[6].matches(':hover')) {
+                options[6].style.visibility = 'hidden';
+                options[6].style.display = "none";
+            }
         }
-    })
+    }
 });
-
-
-
 
