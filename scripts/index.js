1105555be59d79f8d1122978a6232533d5eef52c
metehandoenmez <metehandoenmez@gmail.com>
2023-05-25 17:49:10 +0300

    fixed magicball.svg

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+//pageup-button
+
 const pageUp = document.querySelector(".page-up");
 
 window.addEventListener("scroll", () => {
@@ -8,3 +10,27 @@ window.addEventListener("scroll", () => {
     pageUp.style.display = 'none';
   }
 });
+
+//div 6 table switch
+
+const visibleTable = document.getElementById("div6_visible")
+const hiddenTable = document.getElementById("div6_hidden")
+const switchButton = document.getElementById("div6_switch");
+const title = document.getElementById("d6farms");
+
+function switchTable() {
+  if (visibleTable.style.display === "none") {
+    visibleTable.style.display = "grid";
+    hiddenTable.style.display = "none";
+    title.textContent = "Farms";
+
+  } else {
+    visibleTable.style.display = "none";
+    hiddenTable.style.display = "grid";
+    title.textContent = "Syrup Pools";
+  }
+}
+
+switchButton.addEventListener("click", switchTable);
+
+setInterval(switchTable, 5000);
