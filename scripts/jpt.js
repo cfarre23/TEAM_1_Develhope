ea057cfaa6b7b11f9cf95dd49c097a66ce88b98d
metehandoenmez <metehandoenmez@gmail.com>
2023-05-25 13:53:36 +0300

    changed jpt.js

--- scripts/jpt.js
+++ scripts/jpt.js
@@ -183,17 +183,3 @@ toggleButton.addEventListener("change", () => {
 })
 
 // Fred Homepage Section ends
-
-const pageUp = document.querySelector('.page-up');
-
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 450) {
-        pageUp.style.display = 'flex';
-        pageUp.style.visibility =  'visible'
-    } else {
-        pageUp.style.display = 'none';
-    }
-});
-
-
-
